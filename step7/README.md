8b534c61bc7ef71cfe407f649bce94901a84333d
k02ka <noa_0x00@hotmail.com>
2023-12-21 14:57:23 +0900

    step7 outputを実行

--- step7/README.md
+++ step7/README.md
@@ -1,3 +1,20 @@
 # Step7: Outputs
 https://developer.hashicorp.com/terraform/tutorials/aws-get-started/aws-outputs
 
+## 前提条件
+step6までの作業を終わらせてること
+
+## 概要
+指定の情報を出力できるようにする、今回はinstanceのIDとそのインスタンスのpublic ip
+出力設定はoutputs.tfに記述
+
+applyを実行した後にoutputs.tfで記述した内容が出力
+```
+$ terraform apply
+```
+
+指定の情報のみを取得したい場合はoutputを実行する
+```
+$ terraform output
+```
+
---
+++ step7/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-07c589821f2b353aa"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
